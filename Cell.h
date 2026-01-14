b27c7196960252fe10c989bc5338dda0bd87be28
Ashhal Wallani <wallani.ash06@gmail.com>
2026-01-14 16:07:50 -0500

    added parent pointer and cost

--- Cell.h
+++ Cell.h
@@ -4,9 +4,13 @@
 struct Cell {
     int row;
     int col;
+
     bool isWall = false;
     bool visited = false;
-    int distance = -1;
+
+    int distance = 1e9;      // for Dijkstra / A*
+    int h = 0;       // for A*
+    Cell* parent = nullptr;  // for path reconstruction
 };
 
 #endif
